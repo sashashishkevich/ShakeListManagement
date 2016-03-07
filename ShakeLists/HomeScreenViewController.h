6928bceaf0de891bc0353afe66ed8d774d1a8af1
sashashishkevich <sasha.shishkevich1991@outlook.com>
2016-03-07 05:51:08 -0800

    add the home screen page

---
+++ ShakeLists/HomeScreenViewController.h
@@ -0,0 +1,19 @@
+//
+//  HomeScreenViewController.h
+//  ShakeLists
+//
+//  Created by Software Superstar on 3/7/16.
+//  Copyright © 2016 Software Superstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *circleView;
+
+- (IBAction)createButtonClicked:(id)sender;
+- (IBAction)takeButtonClicked:(id)sender;
+- (IBAction)inviteButtonClicked:(id)sender;
+
+@end
